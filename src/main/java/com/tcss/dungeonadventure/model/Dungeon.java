1a9f7c07d69e4119c35aefc3454118809514171b
hieud201 <hieud201@uw.edu>
2023-10-27 23:04:58 -0700

    First attempt at implementing Dungeon.java

--- src/main/java/com/tcss/dungeonadventure/model/Dungeon.java
+++ src/main/java/com/tcss/dungeonadventure/model/Dungeon.java
@@ -1,4 +1,99 @@
 package com.tcss.dungeonadventure.model;
 
+/**
+ * Represents a randomly generated maze of {@link Room}.
+ * @author Aaron, Sunny, Hieu
+ * @version TCSS 360, Fall 2023
+ */
 public class Dungeon {
+    /**
+     * The 2D representation of the dungeon.
+     */
+    private final Room[][] myMaze;
+
+    /**
+     * The entrance of the dungeon
+     */
+    private final Room myStartingRoom;
+
+    /**
+     * The exit of the dungeon
+     */
+    private final Room myExitRoom;
+
+    /**
+     * The room that contains a pillar of Object-Oriented
+     */
+    private final Room myPillarRoom;
+
+    /**
+     * The current player's location
+     */
+    private Room myCharacterLocation;
+
+    /**
+     * Initializes a 6x6 traversable dungeon.
+     * @param theMaze the maze of rooms of size 6x6
+     * @param theStartingRoom the entrance of the dungeon
+     * @param theExitRoom the exit of the dungeon
+     * @param thePillarRoom the room that contains a pillar of Object-Oriented
+     */
+    public Dungeon(final Room[][] theMaze,
+                   final Room theStartingRoom,
+                   final Room theExitRoom,
+                   final Room thePillarRoom) {
+        myMaze = theMaze;
+        myStartingRoom = theStartingRoom;
+        myExitRoom = theExitRoom;
+        myPillarRoom = thePillarRoom;
+        //How do we go about initializing the starting, exiting, and pillar Rooms?
+        generateDungeon();
+    }
+
+    /**
+     * Constructs a random-generated maze of rooms
+     * with a traversable path from the entrance to the exit
+     * and 4 pillars of Object-Oriented randomly placed in the maze.
+     */
+    private void generateDungeon() {
+        //TODO: implement the algorithm to generate the Dungeon
+        for (int row = 0; row < myMaze.length; row++) {
+            for (int col = 0; col < myMaze[row].length; col++) {
+                myMaze[row][col] =
+            }
+        }
+    }
+
+    /**
+     * Returns a maze of rooms.
+     * @return a maze of rooms
+     */
+    public Room[][] getRooms() {
+        return myMaze;
+    }
+
+    /**
+     * Returns the player's current location in the Dungeon.
+     * @return the player's current location in the Dungeon
+     */
+    public Room getCharacterLocation() {
+        return myCharacterLocation;
+    }
+
+    /**
+     * Returns the room in the maze at the specified coordinates.
+     * @param theX the x-coordinate of the room
+     * @param theY the y-coordinate of the room
+     * @return the room in the maze at the specified coordinates
+     */
+    public Room getRoomAt(final int theX, final int theY) {
+        return myMaze[theX][theY];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        //TODO: implement operations to build the String representation of Dungeon
+        return stringBuilder.toString();
+    }
 }

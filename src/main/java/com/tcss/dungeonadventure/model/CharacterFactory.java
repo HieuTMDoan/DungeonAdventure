43ceddc67819168a4c9080e55a95b0d6e8557bf7
hieud201 <hieud201@uw.edu>
2023-11-09 21:20:30 -0800

    Deleted Factory classes (I'm very sorry, Sunny)

--- src/main/java/com/tcss/dungeonadventure/model/CharacterFactory.java
+++
@@ -1,7 +0,0 @@
-package com.tcss.dungeonadventure.model;
-
-import com.tcss.dungeonadventure.objects.DungeonCharacter;
-
-public interface CharacterFactory {
-    DungeonCharacter createCharacter();
-}

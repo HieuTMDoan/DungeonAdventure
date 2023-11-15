bc884e4df35ad2abaa48897e643deee12c5ede3f
hieud201 <hieud201@uw.edu>
2023-11-14 20:38:16 -0800

    added MenuBarGUI.java to the view folder

---
+++ src/main/java/com/tcss/dungeonadventure/view/MenuBarGUI.java
@@ -0,0 +1,33 @@
+package com.tcss.dungeonadventure.view;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Represents the menu bar of the program's GUI.
+ *
+ * @author Aaron, Sunny, Hieu
+ * @version TCSS 360: Fall 2023
+ */
+public class MenuBarGUI implements ActionListener {
+    /**
+     * Initializes a basic menu bar with 3 menu options.
+     */
+    public MenuBarGUI() {
+
+    }
+
+    /**
+     * Builds the menu bar.
+     */
+    private void buildMenuBar() {
+
+    }
+
+
+    @Override
+    public void actionPerformed(final ActionEvent theEvent) {
+
+    }
+}

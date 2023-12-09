76664dd2b6ecd2f209c62dea97a1116f506c55ca
hieud201 <hieud201@uw.edu>
2023-12-08 22:24:29 -0800

    reconfigured test classes to be testable

---
+++ src/main/tests/GameStateManagerTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameStateManagerTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void createMemento() {
+    }
+
+    @Test
+    void restoreFromMemento() {
+    }
+
+    @Test
+    void getMemento() {
+    }
+
+    @Test
+    void setMemento() {
+    }
+}
---
+++ src/main/tests/PlayerTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getPlayerName() {
+    }
+
+    @Test
+    void getPlayerHero() {
+    }
+
+    @Test
+    void getInventory() {
+    }
+
+    @Test
+    void addItemToInventory() {
+    }
+
+    @Test
+    void removeItemFromInventory() {
+    }
+
+    @Test
+    void hasAllPillars() {
+    }
+}

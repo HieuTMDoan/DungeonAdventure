89d3e3bdb6567e559962ffd28cf0eea19844e714
hieud201 <hieud201@uw.edu>
2023-12-10 03:37:02 -0800

    re-added already-written test cases

--- src/main/tests/com/tcss/dungeonadventure/model/PlayerTest.java
+++ src/main/tests/com/tcss/dungeonadventure/model/PlayerTest.java
@@ -1,5 +1,12 @@
 package com.tcss.dungeonadventure.model;
 
+import com.tcss.dungeonadventure.Helper;
+import com.tcss.dungeonadventure.model.factories.HeroFactory;
+import com.tcss.dungeonadventure.objects.heroes.Hero;
+import com.tcss.dungeonadventure.objects.items.*;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,120 +14,81 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @Test
-    void getPlayerName() {
+    public void getPlayerName() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+
+        final String name = "testPlayer";
+        final Player player = new Player(name, hero);
+        assertEquals(name, player.getPlayerName());
     }
 
     @Test
-    void getPlayerHero() {
+    public void getPlayerHero() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+
+        final Player player = new Player("testPlayer", hero);
+        assertSame(hero, player.getPlayerHero());
     }
 
     @Test
-    void getInventory() {
+    public void getInventory() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+        final Player player = new Player("testPlayer", hero);
+
+
+        final Map<Item, Integer> inventory = player.getInventory();
+
+        assertEquals(0, inventory.size());
+        player.addItemToInventory(new VisionPotion());
+        assertEquals(1, inventory.size());
+
     }
 
     @Test
-    void addItemToInventory() {
+    public void addItemToInventory() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+        final Player player = new Player("testPlayer", hero);
+
+
+        final Map<Item, Integer> inventory = player.getInventory();
+        assertEquals(0, inventory.size());
+        player.addItemToInventory(new VisionPotion());
+        assertEquals(1, inventory.size());
     }
 
     @Test
-    void removeItemFromInventory() {
+    public void removeItemFromInventory() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+        final Player player = new Player("testPlayer", hero);
+
+        final Map<Item, Integer> inventory = player.getInventory();
+        assertEquals(0, inventory.size());
+        player.addItemToInventory(new VisionPotion());
+        assertEquals(1, inventory.size());
+        player.removeItemFromInventory(new VisionPotion());
+        assertEquals(0, inventory.size());
     }
 
     @Test
-    void hasAllPillars() {
+    public void hasAllPillars() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+        final Player player = new Player("testPlayer", hero);
+        player.addItemToInventory(new PillarOfPolymorphism());
+        player.addItemToInventory(new PillarOfEncapsulation());
+        player.addItemToInventory(new PillarOfInheritance());
+        player.addItemToInventory(new PillarOfAbstraction());
+
+        assertTrue(player.hasAllPillars());
+
     }
+
+    @Test
+    public void hasAllPillarsWithoutPillars() {
+        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
+        final Player player = new Player("testPlayer", hero);
+        assertFalse(player.hasAllPillars());
+    }
+
+
+
 }
-
-//import com.tcss.dungeonadventure.Helper;
-//import com.tcss.dungeonadventure.model.Player;
-//import com.tcss.dungeonadventure.model.factories.HeroFactory;
-//import com.tcss.dungeonadventure.objects.heroes.Hero;
-//import com.tcss.dungeonadventure.objects.items.*;
-//import org.junit.Test;
-//
-//import java.util.Map;
-//
-//import static org.junit.Assert.*;
-//
-//
-//public class PlayerTest {
-//
-//    @Test
-//    public void getPlayerName() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//
-//        final String name = "testPlayer";
-//        final Player player = new Player(name, hero);
-//        assertEquals(name, player.getPlayerName());
-//    }
-//
-//    @Test
-//    public void getPlayerHero() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//
-//        final Player player = new Player("testPlayer", hero);
-//        assertSame(hero, player.getPlayerHero());
-//    }
-//
-//    @Test
-//    public void getInventory() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//        final Player player = new Player("testPlayer", hero);
-//
-//
-//        final Map<Item, Integer> inventory = player.getInventory();
-//
-//        assertEquals(0, inventory.size());
-//        player.addItemToInventory(new VisionPotion());
-//        assertEquals(1, inventory.size());
-//
-//    }
-//
-//    @Test
-//    public void addItemToInventory() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//        final Player player = new Player("testPlayer", hero);
-//
-//
-//        final Map<Item, Integer> inventory = player.getInventory();
-//        assertEquals(0, inventory.size());
-//        player.addItemToInventory(new VisionPotion());
-//        assertEquals(1, inventory.size());
-//    }
-//
-//    @Test
-//    public void removeItemFromInventory() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//        final Player player = new Player("testPlayer", hero);
-//
-//        final Map<Item, Integer> inventory = player.getInventory();
-//        assertEquals(0, inventory.size());
-//        player.addItemToInventory(new VisionPotion());
-//        assertEquals(1, inventory.size());
-//        player.removeItemFromInventory(new VisionPotion());
-//        assertEquals(0, inventory.size());
-//    }
-//
-//    @Test
-//    public void hasAllPillars() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//        final Player player = new Player("testPlayer", hero);
-//        player.addItemToInventory(new PillarOfPolymorphism());
-//        player.addItemToInventory(new PillarOfEncapsulation());
-//        player.addItemToInventory(new PillarOfInheritance());
-//        player.addItemToInventory(new PillarOfAbstraction());
-//
-//        assertTrue(player.hasAllPillars());
-//
-//    }
-//
-//    @Test
-//    public void hasAllPillarsWithoutPillars() {
-//        final Hero hero = HeroFactory.createCharacter(Helper.Characters.WARRIOR);
-//        final Player player = new Player("testPlayer", hero);
-//        assertFalse(player.hasAllPillars());
-//    }
-//
-//
-//
-//}
--- src/main/tests/com/tcss/dungeonadventure/model/RoomTest.java
+++ src/main/tests/com/tcss/dungeonadventure/model/RoomTest.java
@@ -1,5 +1,13 @@
 package com.tcss.dungeonadventure.model;
 
+import com.tcss.dungeonadventure.Helper;
+import com.tcss.dungeonadventure.objects.Directions;
+import com.tcss.dungeonadventure.objects.TileChars;
+import com.tcss.dungeonadventure.objects.tiles.*;
+
+import java.awt.*;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,369 +15,253 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoomTest {
 
     @Test
-    void generateRandomTileSet() {
+    public void generateRandomTileSetHasEntrance() {
+        final Tile[][] tiles
+                = Room.generateRandomTileSet(true, false, null);
+
+        assertTrue(Arrays.stream(tiles).flatMap(Arrays::stream).anyMatch(t -> t.getDisplayChar() == TileChars.Room.ENTRANCE));
     }
 
     @Test
-    void addExtraWalls() {
+    public void generateRandomTileSetHasExit() {
+        final Tile[][] tiles
+                = Room.generateRandomTileSet(false, true, null);
+
+        assertTrue(Arrays.stream(tiles).flatMap(Arrays::stream).anyMatch(t -> t.getDisplayChar() == TileChars.Room.EXIT));
     }
 
     @Test
-    void addDoorToWall() {
+    public void generateRandomTileSetBothEntranceExitException() {
+        Object e = null;
+        try {
+            Room.generateRandomTileSet(true, true, null);
+        } catch (final IllegalArgumentException ex) {
+            e = ex;
+        }
+        assertTrue(e instanceof IllegalArgumentException);
     }
 
     @Test
-    void contains() {
+    public void addExtraWalls() {
+        final Room room = new Room(false, false, null);
+
+        final long wallCountBeforeMethod =
+                Arrays.stream(room.getRoomTiles()).flatMap(Arrays::stream).filter(t -> t instanceof WallTile).count();
+
+        Room.addExtraWalls(room);
+
+        final long wallCountAfterMethod =
+                Arrays.stream(room.getRoomTiles()).flatMap(Arrays::stream).filter(t -> t instanceof WallTile).count();
+
+        assertTrue(wallCountBeforeMethod < wallCountAfterMethod);
     }
 
     @Test
-    void movePlayer() {
+    public void addDoorToWall() {
+        final Room room1 = new Room(false, false, null);
+        final Room room2 = new Room(false, false, null);
+
+        room1.addDoorToWall(Directions.Cardinal.SOUTH, room2);
+        room2.addDoorToWall(Directions.Cardinal.NORTH, room1);
+
+        boolean foundFromRoom1 = false;
+        boolean foundFromRoom2 = false;
+        for (final Tile t : room1.getRoomTiles()[room1.getRoomHeight() - 1]) {
+            if (t instanceof DoorTile) {
+                foundFromRoom1 = true;
+                break;
+            }
+        }
+
+        for (final Tile t : room2.getRoomTiles()[0]) {
+            if (t instanceof DoorTile) {
+                foundFromRoom2 = true;
+                break;
+            }
+        }
+
+        assertTrue(foundFromRoom1 && foundFromRoom2);
     }
 
     @Test
-    void findDoorOnWall() {
+    public void contains() {
+        final Tile[][] tiles = new Tile[][]{
+                {new WallTile(), new WallTile(), new WallTile(), new WallTile()},
+                {new WallTile(), new EmptyTile(), new EmptyTile(), new WallTile()},
+                {new WallTile(), new PitTile(), new EmptyTile(), new WallTile()},
+                {new WallTile(), new WallTile(), new WallTile(), new WallTile()}
+        };
+
+        final Room room = new Room(tiles);
+
+        assertTrue(room.contains(TileChars.Room.PIT));
+        assertFalse(room.contains(TileChars.Room.HORIZONTAL_DOOR));
     }
 
     @Test
-    void setPlayerLocation() {
+    public void movePlayer() {
     }
 
     @Test
-    void testSetPlayerLocation() {
+    public void findDoorOnWall() {
+        final Room room1 = new Room(false, false, null);
+        final Room room2 = new Room(false, false, null);
+
+        room1.addDoorToWall(Directions.Cardinal.SOUTH, room2);
+        room2.addDoorToWall(Directions.Cardinal.NORTH, room1);
+
+        assertNotNull(room1.findDoorOnWall(Directions.Cardinal.SOUTH));
+        assertNotNull(room2.findDoorOnWall(Directions.Cardinal.NORTH));
     }
 
     @Test
-    void getDungeonLocation() {
+    public void setPlayerLocation() {
+        final Room room = new Room(false, false, null);
+        final Point p = new Point(1, 1);
+        room.setPlayerLocation(new Point(1, 1));
+        assertEquals(p, new Point(room.getPlayerXPosition(), room.getPlayerYPosition()));
     }
 
     @Test
-    void setDungeonLocation() {
+    public void testSetPlayerLocation() {
     }
 
     @Test
-    void getAdjacentRoomByDirection() {
+    public void getDungeonLocation() {
     }
 
     @Test
-    void testGetAdjacentRoomByDirection() {
+    public void setDungeonLocation() {
     }
 
     @Test
-    void isEntranceRoom() {
+    public void getAdjacentRoomByDirection() {
     }
 
     @Test
-    void isExitRoom() {
+    public void testGetAdjacentRoomByDirection() {
     }
 
     @Test
-    void getRoomWidth() {
+    public void isEntranceRoom() {
+        final Room room = new Room(true, false, null);
+        assertTrue(room.isEntranceRoom());
     }
 
     @Test
-    void getRoomHeight() {
+    public void isEntranceRoomNonEntrance() {
+        final Room room = new Room(false, false, null);
+        assertFalse(room.isEntranceRoom());
     }
 
     @Test
-    void deepCopyRoomData() {
+    public void isExitRoom() {
+        final Room room = new Room(false, true, null);
+        assertTrue(room.isExitRoom());
     }
 
     @Test
-    void createMemento() {
+    public void isExitRoomNonExit() {
+        final Room room = new Room(false, true, null);
+        assertFalse(room.isExitRoom());
     }
 
     @Test
-    void saveToMemento() {
+    public void getRoomWidth() {
+        final int width = Helper.getRandomIntBetween(3, 6);
+        final int height = Helper.getRandomIntBetween(3, 6);
+
+        final Tile[][] tiles = new Tile[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                tiles[i][j] = new EmptyTile();
+            }
+        }
+
+        final Room room = new Room(tiles);
+
+        assertEquals(width, (int) room.getRoomWidth());
     }
 
     @Test
-    void restoreFromMemento() {
+    public void getRoomHeight() {
+        final int width = Helper.getRandomIntBetween(3, 6);
+        final int height = Helper.getRandomIntBetween(3, 6);
+
+        final Tile[][] tiles = new Tile[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                tiles[i][j] = new EmptyTile();
+            }
+        }
+
+        final Room room = new Room(tiles);
+
+        assertEquals(height, (int) room.getRoomHeight());
     }
 
     @Test
-    void getPlayerXPosition() {
+    public void deepCopyRoomData() {
     }
 
     @Test
-    void getPlayerYPosition() {
+    public void createMemento() {
     }
 
     @Test
-    void getPillar() {
+    public void saveToMemento() {
     }
 
     @Test
-    void getRoomTiles() {
+    public void restoreFromMemento() {
     }
 
     @Test
-    void testToString() {
+    public void getPlayerXPosition() {
+    }
+
+    @Test
+    public void getPlayerYPosition() {
+    }
+
+    @Test
+    public void getPillar() {
+        for (final Class<?> pillar : Helper.getPillarList()) {
+            final Room room = new Room(false, false, pillar);
+            assertEquals(pillar, room.getPillar().getClass());
+        }
+    }
+
+    @Test
+    public void getRoomTiles() {
+        final Room room = new Room(false, false, null);
+        assertNotNull(room.getRoomTiles());
+    }
+
+    @Test
+    public void testToString() {
+
+        final Tile[][] tiles = new Tile[][]{
+                {new WallTile(), new WallTile(), new WallTile(), new WallTile()},
+                {new WallTile(), new EmptyTile(), new EmptyTile(), new WallTile()},
+                {new WallTile(), new PitTile(), new EmptyTile(), new WallTile()},
+                {new WallTile(), new WallTile(), new WallTile(), new WallTile()}
+        };
+
+        final Room room = new Room(tiles);
+
+        final StringBuilder sb = new StringBuilder();
+        for (final Tile[] row : tiles) {
+            String prefix = "";
+            for (final Tile tile : row) {
+                sb.append(prefix).append(tile.getDisplayChar());
+                prefix = " ";
+            }
+            sb.append("\n");
+        }
+
+        assertEquals(room.toString(), sb.toString());
+
+
     }
 }
-
-//import com.tcss.dungeonadventure.Helper;
-//import com.tcss.dungeonadventure.model.Room;
-//import com.tcss.dungeonadventure.objects.Directions;
-//import com.tcss.dungeonadventure.objects.TileChars;
-//import com.tcss.dungeonadventure.objects.tiles.*;
-//import org.junit.Test;
-//
-//import java.awt.Point;
-//import java.util.Arrays;
-//
-//import static org.junit.Assert.*;
-//
-//
-//public class RoomTest {
-//
-//    @Test
-//    public void generateRandomTileSetHasEntrance() {
-//        final Tile[][] tiles
-//                = Room.generateRandomTileSet(true, false, null);
-//
-//        assertTrue(Arrays.stream(tiles).flatMap(Arrays::stream).anyMatch(t -> t.getDisplayChar() == TileChars.Room.ENTRANCE));
-//    }
-//
-//    @Test
-//    public void generateRandomTileSetHasExit() {
-//        final Tile[][] tiles
-//                = Room.generateRandomTileSet(false, true, null);
-//
-//        assertTrue(Arrays.stream(tiles).flatMap(Arrays::stream).anyMatch(t -> t.getDisplayChar() == TileChars.Room.EXIT));
-//    }
-//
-//    @Test
-//    public void generateRandomTileSetBothEntranceExitException() {
-//        Object e = null;
-//        try {
-//            Room.generateRandomTileSet(true, true, null);
-//        } catch (final IllegalArgumentException ex) {
-//            e = ex;
-//        }
-//        assertTrue(e instanceof IllegalArgumentException);
-//    }
-//
-//    @Test
-//    public void addExtraWalls() {
-//        final Room room = new Room(false, false, null);
-//
-//        final long wallCountBeforeMethod =
-//                Arrays.stream(room.getRoomTiles()).flatMap(Arrays::stream).filter(t -> t instanceof WallTile).count();
-//
-//        Room.addExtraWalls(room);
-//
-//        final long wallCountAfterMethod =
-//                Arrays.stream(room.getRoomTiles()).flatMap(Arrays::stream).filter(t -> t instanceof WallTile).count();
-//
-//        assertTrue(wallCountBeforeMethod < wallCountAfterMethod);
-//    }
-//
-//    @Test
-//    public void addDoorToWall() {
-//        final Room room1 = new Room(false, false, null);
-//        final Room room2 = new Room(false, false, null);
-//
-//        room1.addDoorToWall(Directions.Cardinal.SOUTH, room2);
-//        room2.addDoorToWall(Directions.Cardinal.NORTH, room1);
-//
-//        boolean foundFromRoom1 = false;
-//        boolean foundFromRoom2 = false;
-//        for (final Tile t : room1.getRoomTiles()[room1.getRoomHeight() - 1]) {
-//            if (t instanceof DoorTile) {
-//                foundFromRoom1 = true;
-//                break;
-//            }
-//        }
-//
-//        for (final Tile t : room2.getRoomTiles()[0]) {
-//            if (t instanceof DoorTile) {
-//                foundFromRoom2 = true;
-//                break;
-//            }
-//        }
-//
-//        assertTrue(foundFromRoom1 && foundFromRoom2);
-//    }
-//
-//    @Test
-//    public void contains() {
-//        final Tile[][] tiles = new Tile[][]{
-//                {new WallTile(), new WallTile(), new WallTile(), new WallTile()},
-//                {new WallTile(), new EmptyTile(), new EmptyTile(), new WallTile()},
-//                {new WallTile(), new PitTile(), new EmptyTile(), new WallTile()},
-//                {new WallTile(), new WallTile(), new WallTile(), new WallTile()}
-//        };
-//
-//        final Room room = new Room(tiles);
-//
-//        assertTrue(room.contains(TileChars.Room.PIT));
-//        assertFalse(room.contains(TileChars.Room.HORIZONTAL_DOOR));
-//    }
-//
-//    @Test
-//    public void movePlayer() {
-//    }
-//
-//    @Test
-//    public void findDoorOnWall() {
-//        final Room room1 = new Room(false, false, null);
-//        final Room room2 = new Room(false, false, null);
-//
-//        room1.addDoorToWall(Directions.Cardinal.SOUTH, room2);
-//        room2.addDoorToWall(Directions.Cardinal.NORTH, room1);
-//
-//        assertNotNull(room1.findDoorOnWall(Directions.Cardinal.SOUTH));
-//        assertNotNull(room2.findDoorOnWall(Directions.Cardinal.NORTH));
-//    }
-//
-//    @Test
-//    public void setPlayerLocation() {
-//        final Room room = new Room(false, false, null);
-//        final Point p = new Point(1, 1);
-//        room.setPlayerLocation(new Point(1, 1));
-//        assertEquals(p, new Point(room.getPlayerXPosition(), room.getPlayerYPosition()));
-//    }
-//
-//    @Test
-//    public void testSetPlayerLocation() {
-//    }
-//
-//    @Test
-//    public void getDungeonLocation() {
-//    }
-//
-//    @Test
-//    public void setDungeonLocation() {
-//    }
-//
-//    @Test
-//    public void getAdjacentRoomByDirection() {
-//    }
-//
-//    @Test
-//    public void testGetAdjacentRoomByDirection() {
-//    }
-//
-//    @Test
-//    public void isEntranceRoom() {
-//        final Room room = new Room(true, false, null);
-//        assertTrue(room.isEntranceRoom());
-//    }
-//
-//    @Test
-//    public void isEntranceRoomNonEntrance() {
-//        final Room room = new Room(false, false, null);
-//        assertFalse(room.isEntranceRoom());
-//    }
-//
-//    @Test
-//    public void isExitRoom() {
-//        final Room room = new Room(false, true, null);
-//        assertTrue(room.isExitRoom());
-//    }
-//
-//    @Test
-//    public void isExitRoomNonExit() {
-//        final Room room = new Room(false, true, null);
-//        assertFalse(room.isExitRoom());
-//    }
-//
-//    @Test
-//    public void getRoomWidth() {
-//        final int width = Helper.getRandomIntBetween(3, 6);
-//        final int height = Helper.getRandomIntBetween(3, 6);
-//
-//        final Tile[][] tiles = new Tile[width][height];
-//        for (int i = 0; i < width; i++) {
-//            for (int j = 0; j < height; j++) {
-//                tiles[i][j] = new EmptyTile();
-//            }
-//        }
-//
-//        final Room room = new Room(tiles);
-//
-//        assertEquals(width, (int) room.getRoomWidth());
-//    }
-//
-//    @Test
-//    public void getRoomHeight() {
-//        final int width = Helper.getRandomIntBetween(3, 6);
-//        final int height = Helper.getRandomIntBetween(3, 6);
-//
-//        final Tile[][] tiles = new Tile[width][height];
-//        for (int i = 0; i < width; i++) {
-//            for (int j = 0; j < height; j++) {
-//                tiles[i][j] = new EmptyTile();
-//            }
-//        }
-//
-//        final Room room = new Room(tiles);
-//
-//        assertEquals(height, (int) room.getRoomHeight());
-//    }
-//
-//    @Test
-//    public void deepCopyRoomData() {
-//    }
-//
-//    @Test
-//    public void createMemento() {
-//    }
-//
-//    @Test
-//    public void saveToMemento() {
-//    }
-//
-//    @Test
-//    public void restoreFromMemento() {
-//    }
-//
-//    @Test
-//    public void getPlayerXPosition() {
-//    }
-//
-//    @Test
-//    public void getPlayerYPosition() {
-//    }
-//
-//    @Test
-//    public void getPillar() {
-//        for (final Class<?> pillar : Helper.getPillarList()) {
-//            final Room room = new Room(false, false, pillar);
-//            assertEquals(pillar, room.getPillar().getClass());
-//        }
-//    }
-//
-//    @Test
-//    public void getRoomTiles() {
-//        final Room room = new Room(false, false, null);
-//        assertNotNull(room.getRoomTiles());
-//    }
-//
-//    @Test
-//    public void testToString() {
-//
-//        final Tile[][] tiles = new Tile[][]{
-//                {new WallTile(), new WallTile(), new WallTile(), new WallTile()},
-//                {new WallTile(), new EmptyTile(), new EmptyTile(), new WallTile()},
-//                {new WallTile(), new PitTile(), new EmptyTile(), new WallTile()},
-//                {new WallTile(), new WallTile(), new WallTile(), new WallTile()}
-//        };
-//
-//        final Room room = new Room(tiles);
-//
-//        final StringBuilder sb = new StringBuilder();
-//        for (final Tile[] row : tiles) {
-//            String prefix = "";
-//            for (final Tile tile : row) {
-//                sb.append(prefix).append(tile.getDisplayChar());
-//                prefix = " ";
-//            }
-//            sb.append("\n");
-//        }
-//
-//        assertEquals(room.toString(), sb.toString());
-//
-//
-//    }
-//}

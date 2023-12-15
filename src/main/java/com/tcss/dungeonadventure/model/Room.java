a1aef6f84f48b96c4d38e626643f447650acccd9
Sunny <hassan.sayedlee@gmail.com>
2023-12-15 11:18:32 -0800

    heavily guarded exit

--- src/main/java/com/tcss/dungeonadventure/model/Room.java
+++ src/main/java/com/tcss/dungeonadventure/model/Room.java
@@ -216,8 +216,25 @@ public class Room implements Serializable {
         if (theIsEntrance || theIsExit) {
             putTileAtValidLocation(theIsEntrance ? new EntranceTile()
                     : new ExitTile(), tiles, false);
+
+            // Increase the chance of having more monsters in exit rooms
+            final double monsterRandom = Helper.getRandomDoubleBetween(0, 1);
+            final int monsterNum = (monsterRandom < TWO_MONSTER_CHANCE)
+                    ? 6  // Adjust the number of monsters as needed for a heavily guarded exit
+                    : (monsterRandom < ONE_MONSTER_CHANCE)
+                    ? 5
+                    : 4;
+            for (int i = 0; i < monsterNum; i++) {
+                final Monster randomMonster = Helper.getRandomMonster();
+                putTileAtValidLocation(new NPCTile(randomMonster), tiles, true);
+            }
+
+            // Add extra walls for increased difficulty
+            addExtraWalls(new Room(tiles));
+
             return tiles;
         }
+
 
         if (thePillar != null) {
             try {
@@ -247,10 +264,10 @@ public class Room implements Serializable {
             // Increase the chance of having more monsters in exit rooms
             final double monsterRandom = Helper.getRandomDoubleBetween(0, 1);
             final int monsterNum = (monsterRandom < TWO_MONSTER_CHANCE)
-                    ? 3  // Adjust the number of monsters as needed
+                    ? 4  // Adjust the number of monsters as needed
                     : (monsterRandom < ONE_MONSTER_CHANCE)
-                    ? 2
-                    : 1;
+                    ? 3
+                    : 2;
             for (int i = 0; i < monsterNum; i++) {
                 final Monster randomMonster = Helper.getRandomMonster();
                 putTileAtValidLocation(new NPCTile(randomMonster), tiles, true);
@@ -281,17 +298,7 @@ public class Room implements Serializable {
             final Monster randomMonster = Helper.getRandomMonster();
             putTileAtValidLocation(new NPCTile(randomMonster), tiles, true);
         }
-//        // Increase the overall number of monsters in the dungeon
-//        final double totalMonsterRandom = Helper.getRandomDoubleBetween(0, 1);
-//        final int totalMonsterNum = (totalMonsterRandom < TWO_MONSTER_CHANCE)
-//                ? 5  // Adjust the total number of monsters as needed
-//                : (totalMonsterRandom < ONE_MONSTER_CHANCE)
-//                ? 4
-//                : 3;
-//        for (int i = 0; i < totalMonsterNum; i++) {
-//            final Monster randomMonster = Helper.getRandomMonster();
-//            putTileAtValidLocation(new NPCTile(randomMonster), tiles, true);
-//        }
+
 
         final double pitRandom = Helper.getRandomDoubleBetween(0, 1);
         final int pitNum = (pitRandom < TWO_PIT_CHANCE)
